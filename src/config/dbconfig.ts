f255c613b95f823b00f9d24ae447ea427f712ca0
donteghen <donaldteghen@gmail.com>
2022-07-14 13:44:50 +0100

    Prod test release-03

--- src/config/dbconfig.ts
+++ src/config/dbconfig.ts
@@ -3,6 +3,6 @@ import {connect} from 'mongoose'
 
 export const connectDb = async (): Promise<void> => {
      connect(process.env.MONGO_STRING)
-     .then(() => console.log('Db is connected'))
+     .then(() => console.log('Db is connected and server is ready'))
      .catch(error => console.log(error))
 }
